34ec4deaa02d47c7b2d985e2e4785ded6a9cf3c2
Kris Foss <kris.foss@gmail.com>
2016-07-21 20:44:36 -0600

    build open struct from team info

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,25 @@
 class Team < ActiveRecord::Base
   belongs_to :user
 
+  attr_reader :team_id,
+              :team_name,
+              :team_members
+
+  def self.service
+    @service ||= TeamSnapService.new
+  end
+
+  def service.team
+    service.teams.map { |team| Team.new(team)}
+  end
+
+  def self.find(team_id)
+    Team.new(service.team_members(current_user, team_id))
+  end
+
+  def initialize(data)
+    @team_id   = data["collection"]["items"][0]["data"][0]["value"]
+    @team_name = data["collection"]["items"][0]["data"][14]["value"]
+  end
+
 end
--- app/services/team_snap_service.rb
+++ app/services/team_snap_service.rb
@@ -5,9 +5,17 @@ class TeamSnapService
     @conn = Faraday.new(url: 'https://api.teamsnap.com/v3/')
   end
 
-  def get_team_info(current_user)
+  def teams(current_user)
     response = conn.get do |req|
                 req.url "teams/search?user_id=#{current_user.uid}"
+                req.headers['Authorization'] = "Bearer #{current_user.token}"
+              end
+    parse(response)
+  end
+
+  def team_members(current_user, team_id)
+    response = conn.get do |req|
+                req.url "members/search?team_id=#{team_id}"
                 req.headers['Authorization'] = "Bearer #{current_user.token}"
               end
     parse(response)
