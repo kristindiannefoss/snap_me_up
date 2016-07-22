99897c45161586b7df352b639935efe92c01b5b3
Kris Foss <kris.foss@gmail.com>
2016-07-21 20:26:17 -0600

    set up service to get team info from api

--- app/services/team_snap_service.rb
+++ app/services/team_snap_service.rb
@@ -4,11 +4,19 @@ class TeamSnapService
   def initialize
     @conn = Faraday.new(url: 'https://api.teamsnap.com/v3/')
   end
-#
 
-  def team_info(current_user, auth_token)
-    request = conn.get "teams/search?user_id=#{current_user.uid}", {
-      Authorization: "Bearer #{current_user.token}"
-    }
+  def get_team_info(current_user)
+    response = conn.get do |req|
+                req.url "teams/search?user_id=#{current_user.uid}"
+                req.headers['Authorization'] = "Bearer #{current_user.token}"
+              end
+    parse(response)
   end
+
+  private
+
+    def parse(response)
+      JSON.parse(response.body)
+    end
+
 end
